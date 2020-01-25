72cab8f31b40923b399f7b4d3b77754f720ae916
Agung Wicaksono <wicaksonomm1@gmail.com>
2020-01-25 18:15:39 +0800

    init2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('telp');
+            $table->string('role');
+						$table->timestamps();
+						
         });
     }
 
