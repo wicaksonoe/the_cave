2bb1bee4d08a255525886fba2d217284fc2b489a
wisnusugara <wisnusugara.ws@gmail.com>
2020-01-28 18:27:37 +0800

    tambah barang

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -7,40 +7,46 @@
 @stop
 
 @section('content')
-    <p>
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Data Barang</h3>
+
+@include('tambah-barang')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Data Barang</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <a onclick="$('#tambahBarang').modal('show')"><button type="button" class="btn btn-primary" style="margin-bottom: 10px">
+                            <i class="fa fa-plus-square" aria-hidden="true"></i>Tambah
+                        </button></a>
+
+                        <table id="tabel" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Aksi</th>
+                                    <th>Nama Barang</th>
+                                    <th>Jenis</th>
+                                    <th>Tipe</th>
+                                    <th>Jumlah</th>
+                                    <th>Harga Pokok</th>
+                                    <th>Harga Jual</th>
+                                    <th>Tanggal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <a href="?page=tambah-penyakit&level=admin"><button type="button" style="margin-bottom: 10px" class="btn btn-primary">
-                    <i class="fa fa-plus-square" aria-hidden="true"></i> Tambah
-                </button></a>
-                <table id="tabel" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Aksi</th>
-                            <th>Nama Barang</th>
-                            <th>Jenis</th>
-                            <th>Tipe</th>
-                            <th>Jumlah</th>
-                            <th>Harga Pokok</th>
-                            <th>Harga Jual</th>
-                            <th>Tanggal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.card-body -->
         </div>
-        <script>
+    </div>
 
-        </script>
-
-    </p>
 @stop
 
 @section('css')
@@ -49,4 +55,13 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
+    <script>
+        function tambahBarang() {
+            save_method ="tambah";
+            $('input[name=_method]').val('POST');
+            $('#modal-form').modal('show');
+            $('#modal-form form')[0].reset();
+            $('.modal-title').text('Tambah');
+        }
+    </script>
 @stop
