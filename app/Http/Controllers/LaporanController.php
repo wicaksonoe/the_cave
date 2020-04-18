aff4332815ab690f9c2f88e3724f9567cc91586c
Agung Wicaksono <wicaksonomm1@gmail.com>
2020-04-18 22:13:04 +0800

    api laporan daftar barang fix (bazar, global, all)

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Barang_masuk;
 use App\Detail_penjualan;
+use App\Detail_penjualan_bazar;
 use Illuminate\Http\Request;
 
 class LaporanController extends Controller
@@ -12,11 +13,11 @@ class LaporanController extends Controller
     {
         switch ($param) {
             case 'all':
-                # code...
+                return $this->dashboard_all($bulan, $tahun);
                 break;
 
             case 'bazar':
-                # code...
+                return $this->dashboard_bazar($bulan, $tahun);
                 break;
 
             case 'global':
@@ -24,9 +25,58 @@ class LaporanController extends Controller
                 break;
 
             default:
-                # code...
+                return response()->json(
+                    [
+                        'success' => false,
+                        'message' => 'Request not found.'
+                    ],
+                    404
+                );
                 break;
         }
+    }
+
+    public function dashboard_all($bulan, $tahun)
+    {
+        $daftar_barcode_bazar = Detail_penjualan_bazar::whereMonth('created_at', $bulan)
+            ->whereYear('created_at', $tahun)
+            ->selectRaw('barcode, sum(jumlah) as jumlah')
+            ->groupBy('barcode')
+            ->orderBy('jumlah', 'desc')
+            ->get();
+
+        $daftar_barcode_global = Detail_penjualan::whereMonth('created_at', $bulan)
+            ->whereYear('created_at', $tahun)
+            ->selectRaw('barcode, sum(jumlah) as jumlah')
+            ->groupBy('barcode')
+            ->orderBy('jumlah', 'desc')
+            ->get();
+
+        $daftar_barcode_gabungan = $daftar_barcode_bazar->mergeRecursive($daftar_barcode_global);
+
+        $daftar_barcode_laris = $daftar_barcode_gabungan->sortByDesc('jumlah');
+
+        $daftar_barang_laris = [];
+        foreach ($daftar_barcode_laris as $value) {
+            array_push($daftar_barang_laris, [
+                'barcode' => $value->barcode,
+                'nama_barang' => Barang_masuk::where('barcode', $value->barcode)->withTrashed()->first()->namabrg,
+                'jumlah' => $value->jumlah
+            ]);
+
+            if (count($daftar_barang_laris) == 10) {
+                break;
+            }
+        }
+
+        return response()->json(
+            [
+                'success' => true,
+                'jumlah_data' => count($daftar_barang_laris),
+                'data' => $daftar_barang_laris
+            ],
+            200
+        );
     }
 
     public function dashboard_global($bulan, $tahun)
@@ -51,11 +101,43 @@ class LaporanController extends Controller
             ]);
         }
 
-        return response()->json([
-            'success' => true,
-            'data'    => $daftar_barang_laris,
-        ], 200
-    );
+        return response()->json(
+            [
+                'success' => true,
+                'jumlah_data' => count($daftar_barang_laris),
+                'data'    => $daftar_barang_laris,
+            ],
+            200
+        );
+    }
 
+    public function dashboard_bazar($bulan, $tahun)
+    {
+        $daftar_barcode = Detail_penjualan_bazar::whereMonth('created_at', $bulan)
+            ->whereYear('created_at', $tahun)
+            ->selectRaw('barcode, sum(jumlah) as jumlah')
+            ->groupBy('barcode')
+            ->orderBy('jumlah', 'desc')
+            ->limit(10)
+            ->get();
+
+        $daftar_barang_laris = [];
+
+        foreach ($daftar_barcode as $key => $value) {
+            array_push($daftar_barang_laris, [
+                'barcode' => $value->barcode,
+                'nama_barang' => Barang_masuk::where('barcode', $value->barcode)->withTrashed()->first()->namabrg,
+                'jumlah' => $value->jumlah
+            ]);
+        }
+
+        return response()->json(
+            [
+                'success' => true,
+                'jumlah_data' => count($daftar_barang_laris),
+                'data'    => $daftar_barang_laris,
+            ],
+            200
+        );
     }
 }
